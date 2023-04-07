c3c980e34a98b025a0e7f92449479688d7763647
Kacper Łukaszczyk <k.lukaszczyk@gmail.com>
2023-04-07 08:18:25 +0200

    Ad js generate titleLinks

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function titleClickHandler(event){
   event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
+  console.log(event);
 
 
 
@@ -34,12 +35,52 @@ function titleClickHandler(event){
   const targetArticle = document.querySelector(articleSelector);
   console.log(targetArticle);
 
-  /* [IN PROGRESS] add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* [DONE] remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML ='';
+
+  /* [DONE]for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  
+  let html = '';
+
+  for(let article of articles){
+      /* get the article id */
+    const articleId = article.getAttribute('id');
+
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* get the title from the title element */
+    /* ... */
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' +  articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+
+    /* insert link into html variable */
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll('.titles a');
+console.log('poka linki' + links);
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+}
+
+generateTitleLinks();
